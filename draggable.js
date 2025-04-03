84eec81cad6fb677d7dba7d5db1f1567c628b920
AzazeIIo <61389155+AzazeIIo@users.noreply.github.com>
2025-04-03 12:47:48 +0200

    Refactor

---
+++ draggable.js
@@ -0,0 +1,56 @@
+export class Draggable {
+    isDragged;
+    mouseOffsetX;
+    mouseOffsetY;
+    element;
+    
+    constructor(e) {
+        const elem = e.target;
+        const position = elem.getBoundingClientRect();
+
+        this.isDragged = true;
+        this.element = this.clone(elem, position);
+        this.element.addEventListener('mousedown', (ev) => this.mouseDown(ev));
+        this.element.addEventListener('mouseup', () => this.mouseUp());
+        this.element.addEventListener('mousemove', (ev) => this.mouseMove(ev));
+        this.element.style.zIndex = 1;
+
+        const mouseX = e.clientX;
+        const mouseY = e.clientY;
+        this.mouseOffsetX = mouseX - position.left;
+        this.mouseOffsetY = mouseY - position.top;
+    }
+
+    clone(elem, pos) {
+        const draggableElement = elem.cloneNode();
+        draggableElement.style.position = 'absolute';
+        draggableElement.style.top = pos.top+'px';
+        draggableElement.style.left = pos.left+'px';
+        draggableElement.style.backgroundColor = 'red';
+        elem.after(draggableElement);
+
+        return draggableElement;
+    }
+    
+    mouseDown(e) {
+        this.isDragged = true;
+        const position = this.element.getBoundingClientRect();
+        this.mouseOffsetX = e.clientX - position.left;
+        this.mouseOffsetY = e.clientY - position.top;
+        this.element.style.zIndex = 1;
+    }
+
+    mouseUp() {
+        this.isDragged = false;
+        this.element.style.zIndex = 0;
+    };
+
+    mouseMove(e) {
+        if(this.isDragged) {
+            let mouseX = e.clientX;
+            let mouseY = e.clientY;
+            $(this.element).css('top', mouseY - this.mouseOffsetY);
+            $(this.element).css('left', mouseX - this.mouseOffsetX);
+        }
+    };
+}
